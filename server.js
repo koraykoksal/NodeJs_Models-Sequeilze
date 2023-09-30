3aa1aa11cdacbf596d5b9d120a7523f5de84a2d7
koraykoksal <kksl.koray@gmail.com>
2023-09-30 13:17:11 +0300

    created Todo modal

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
+const sqlite3 = process.env.SQLITE || 'db.sqlite3'
 
 /* ------------------------------------------------------- */
 // Accept json data:
@@ -19,6 +20,49 @@ app.all('/', (req, res) => {
 })
 
 // continue from here...
+//* SEQUELIZE
+//? sqlite import edilir
+const {Sequelize, DataTypes} = require('sequelize')
+
+//? sql özelliklerine erişmek için instance oluşturulur
+const sequelize = new Sequelize(`sqlite:./${sqlite3}`)
+
+//? todo isminde modal oluşturulur
+const Todo = sequelize.define('todo',{
+
+    id:{
+        type:DataTypes.INTEGER,
+        allowNull:false, // default true
+        unique:true,
+        field_name:'custom_column_name',
+        comment:'Description',
+        primaryKey:true,
+        autoIncrement:true,
+        
+    },
+    title:{
+        type:DataTypes.STRING(64),
+        allowNull:false,
+
+    },
+    description:{
+        type:DataTypes.TEXT,
+        allowNull:true
+    },
+    priority:{ //? hight:1, normal:0, low:-1
+        type:DataTypes.TINYINT,
+        allowNull:false,
+        defaultValue:0,
+    },
+    isDone:{
+        type:DataTypes.BOOLEAN,
+        allowNull:false,
+        defaultValue:false,
+    }
+})
+
+
+
 
 const errorHandler = (err, req, res, next) => {
     const errorStatusCode = res.errorStatusCode ?? 500
